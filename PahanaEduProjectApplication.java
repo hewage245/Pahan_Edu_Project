8582ad5cec01481ba830597fdc4ccecb1765a624
imasha <hewagesha2@gmail.com>
2025-08-20 03:13:44 -0700

    Add files via upload

---
+++ PahanaEduProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.PahanaEduProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahanaEduProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahanaEduProjectApplication.class, args);
+	}
+
+}
